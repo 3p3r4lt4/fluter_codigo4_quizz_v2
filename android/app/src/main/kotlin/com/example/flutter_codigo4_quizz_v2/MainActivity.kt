d58570ec539a58d0b22d6f1146fafc8120349639
3p3r4lt4 <eduardo7flx@gmail.com>
2023-05-18 12:21:24 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/flutter_codigo4_quizz_v2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_codigo4_quizz_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatefulWidget {
+   MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<Icon> scoreKeeper =[];
+
+  List<String> questions =[
+    "El hombre llego a la luna?",
+    "La tierra es plana?",
+    "Ella te ama",
+  ];
+
+    int questionNumber =0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color(0xff212121),
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children:  [
+              Expanded(
+                flex: 5,
+                child: Center(
+                  child: Text(
+                    questions[questionNumber],
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: MaterialButton(
+                    color: Colors.greenAccent,
+                    child: Text("Verdadero"),
+                    onPressed: (){
+                      questionNumber++;
+                      scoreKeeper.add(Icon(Icons.check,color: Colors.greenAccent,),);
+                      setState(() {
+
+                      });
+                      },
+                  ),
+                ),
+              ),
+
+              Expanded(
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: MaterialButton(
+                    color: Colors.redAccent,
+                    child: Text("Falso"),
+                    onPressed: (){
+                      questionNumber++;
+                      scoreKeeper.add(Icon(Icons.close,color: Colors.redAccent),);
+                      setState(() {
+
+                      });
+                    },
+                  ),
+                ),
+              ),
+
+              Row(
+                children: scoreKeeper,
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
